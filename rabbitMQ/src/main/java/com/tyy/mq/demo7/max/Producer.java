a6626047f2554bdaf2f3f2da5ef02cac03657437
yytang26 <yytangtangyy@outlook.com>
2021-10-11 21:39:34 +0800

    死信队列的三种方式

---
+++ rabbitMQ/src/main/java/com/tyy/mq/demo7/max/Producer.java
@@ -0,0 +1,26 @@
+package com.tyy.mq.demo7.max;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.tyy.mq.util.RabbitMqUtil;
+
+/**
+ * @author:tyy
+ * @date:2021/10/11
+ */
+public class Producer {
+    private static final String NORMAL_EXCHANGE = "normal_exchange";
+
+    public static void main(String[] argv) throws Exception {
+        Channel channel = RabbitMqUtil.getChannel("127.0.0.1", "tyy", "123");
+        channel.exchangeDeclare(NORMAL_EXCHANGE, BuiltinExchangeType.DIRECT);
+
+        //该信息是用作演示队列个数限制
+        for (int i = 1; i < 11; i++) {
+            String message = "info" + i;
+            channel.basicPublish(NORMAL_EXCHANGE, "zhangsan", null, message.getBytes());
+            System.out.println("生产者发送消息:" + message);
+        }
+    }
+}
---
+++ rabbitMQ/src/main/java/com/tyy/mq/demo7/ttl/Consumer01.java
@@ -0,0 +1,52 @@
+package com.tyy.mq.demo7.ttl;
+
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DeliverCallback;
+import com.tyy.mq.util.RabbitMqUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author:tyy
+ * @date:2021/10/11
+ */
+public class Consumer01 {
+
+    //普通交换机名称
+    private static final String NORMAL_EXCHANGE = "normal_exchange";
+    //死信交换机名称
+    private static final String DEAD_EXCHANGE = "dead_exchange";
+
+    public static void main(String[] argv) throws Exception {
+        Channel channel = RabbitMqUtil.getChannel("127.0.0.1", "tyy", "123");
+        //声明死信和普通交换机 类型为 direct
+        channel.exchangeDeclare(NORMAL_EXCHANGE, BuiltinExchangeType.DIRECT);
+        channel.exchangeDeclare(DEAD_EXCHANGE, BuiltinExchangeType.DIRECT);
+        //声明死信队列
+        String deadQueue = "dead-queue";
+        channel.queueDeclare(deadQueue, false, false, false, null);
+        //死信队列绑定死信交换机与 routingkey
+        channel.queueBind(deadQueue, DEAD_EXCHANGE, "lisi");
+        //正常队列绑定死信队列信息
+        Map<String, Object> params = new HashMap<>();
+        //正常队列设置死信交换机 参数 key 是固定值
+        params.put("x-dead-letter-exchange", DEAD_EXCHANGE);
+        //正常队列设置死信 routing-key 参数 key 是固定值
+        params.put("x-dead-letter-routing-key", "lisi");
+
+        String normalQueue = "normal-queue";
+        channel.queueDeclare(normalQueue, false, false, false, params);
+        channel.queueBind(normalQueue, NORMAL_EXCHANGE, "zhangsan");
+
+        System.out.println("等待接收消息 ........... ");
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println("Consumer01 接收到消息" + message);
+        };
+        channel.basicConsume(normalQueue, true, deliverCallback, consumerTag -> {
+        });
+    }
+}
+
